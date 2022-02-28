97feef7411fd190c36ce897de2b8b6b18d9dfdc7
Carl Velasco <carlvelasco96@gmail.com>
2022-02-28 15:52:17 +1300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,16 +13,19 @@ export class AppController {
 
   @Get('set-cookie')
   setCookie(@Res() res: Response): Response {
-    return res.cookie('test', 'Hello World!', {
-      domain: '.createbase.co.nz',
-      expires: new Date(Date.now() + Math.pow(9, 10)),
-      secure: true,
-    });
+    return res
+      .cookie('test', 'Hello World!', {
+        domain: '.createbase.co.nz',
+        expires: new Date(Date.now() + Math.pow(9, 10)),
+        secure: true,
+      })
+      .status(200)
+      .send('Cookie Set');
   }
 
   @Get('get-cookie')
-  getCookie(@Req() req: Request): Request {
-    return req.cookies['test'];
+  getCookie(@Req() req: Request, @Res() res: Response): Response {
+    return res.status(200).send(req.cookies['test']);
   }
 
   @Post('convert-date')
